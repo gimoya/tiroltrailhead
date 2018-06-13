6b86126ef7634defad6cc3d1cb685aede1f074fa
gimoya <kay.cichini@gmail.com>
2018-06-13 08:56:16 +0200

    batch updates from shell script..

--- webmap/digitizing/markupForm.py
+++ webmap/digitizing/markupForm.py
@@ -6,8 +6,6 @@ def formOpen(dialog,layerid,featureid):
 	nameField = dialog.findChild(QLineEdit,"name")
 	global regionField
 	regionField = dialog.findChild(QComboBox,"region")
-	global altField
-	altField = dialog.findChild(QLineEdit,"altitude")
 	global characField
 	characField = dialog.findChild(QLineEdit,"character")
 	global riskField
@@ -24,7 +22,6 @@ def formOpen(dialog,layerid,featureid):
 	descrField = dialog.findChild(QPlainTextEdit,"description")
 	nameField.textChanged.connect( newDescr )
 	regionField.currentIndexChanged.connect( newDescr )
-	altField.textChanged.connect( newDescr )
 	characField.textChanged.connect( newDescr )
 	riskField.textChanged.connect( newDescr )
 	uphillField.textChanged.connect( newDescr )
@@ -33,7 +30,7 @@ def formOpen(dialog,layerid,featureid):
 	conflField.textChanged.connect( newDescr )
 
 def newDescr():
-	descrField.setPlainText('<div id="topic" style="float:left;">Name:</br>Region:</br>Seeh&ouml;he:</br>Charakter:</br>Risiko:</br>Uphill Pain:</br>Landschaft:</br>Konflikt:</br>Aufstiegshilfe:</div><div id="topic-text">' +	
-	nameField.text() + '</br>' + regionField.currentText() + '</br>' + altField.text() + '</br>' +
+	descrField.setPlainText('<div id="topic" style="float:left;">Name:</br>Region:</br>Charakter:</br>Risiko:</br>Uphill Pain:</br>Landschaft:</br>Konflikt:</br>Aufstiegshilfe:</div><div id="topic-text">' +	
+	nameField.text() + '</br>' + regionField.currentText() + '</br>' +
 	characField.text() + '</br>' + riskField.text() + '</br>' + uphillField.text() + '</br>' + 
 	landscField.text() + '</br>' + conflField.text() + '</br>' + shuttleField.text() + '</div>')
