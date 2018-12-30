cd5bb957e410a5d3c34f3b93ec87cc768f32ba40
gimoya <kay.cichini@gmail.com>
2018-12-30 18:46:08 +0100

    batch updates from shell script..

--- webmap/leaflet_map/leaflet_map_1.js
+++ webmap/leaflet_map/leaflet_map_1.js
@@ -77,35 +77,36 @@ function styleLines(feature) {
 		
 var trailsLayer;
 
-//all used options are the default values
-var el = L.control.elevation({
-  	position: "topright",
-	theme: "steelblue-theme", //default: lime-theme
-	width: 600,
-	height: 125,
-	margins: {
-		top: 10,
-		right: 20,
-		bottom: 30,
-		left: 50
-	},
-	useHeightIndicator: true, //if false a marker is drawn at map position
-	interpolation: "linear", //see https://github.com/mbostock/d3/wiki/SVG-Shapes#wiki-area_interpolate
-	hoverNumber: {
-		decimalsX: 3, //decimals on distance (always in km)
-		decimalsY: 0, //deciamls on hehttps://www.npmjs.com/package/leaflet.coordinatesight (always in m)
-		formatter: undefined //custom formatter function may be injected
-	},
-	xTicks: undefined, //number of ticks in x axis, calculated by default according to width
-	yTicks: undefined, //number of ticks on y axis, calculated by default according to height
-	collapsed: true,  //collapsed mode, show chart on click or mouseover
-	imperial: false    //display imperial units instead of metric
-});
 
-map.addControl(el);
-
-var save_event;
-	
+function addData(e) {
+	//all used options are the default values
+	var el = L.control.elevation({
+		position: "topright",
+		theme: "steelblue-theme", //default: lime-theme
+		width: 600,
+		height: 125,
+		margins: {
+			top: 10,
+			right: 20,
+			bottom: 30,
+			left: 50
+		},
+		useHeightIndicator: true, //if false a marker is drawn at map position
+		interpolation: "linear", //see https://github.com/mbostock/d3/wiki/SVG-Shapes#wiki-area_interpolate
+		hoverNumber: {
+			decimalsX: 3, //decimals on distance (always in km)
+			decimalsY: 0, //deciamls on hehttps://www.npmjs.com/package/leaflet.coordinatesight (always in m)
+			formatter: undefined //custom formatter function may be injected
+		},
+		xTicks: undefined, //number of ticks in x axis, calculated by default according to width
+		yTicks: undefined, //number of ticks on y axis, calculated by default according to height
+		collapsed: true,  //collapsed mode, show chart on click or mouseover
+		imperial: false    //display imperial units instead of metric
+	});
+    el.addData(e);
+    map.addControl(el);
+}
+		
 $.getJSON('Trails.json', function(json) {
   trailsLayer = L.geoJson(json, {
     style: styleLines,
@@ -116,8 +117,7 @@ $.getJSON('Trails.json', function(json) {
 		layer.bindPopup(popupContent, trailPopupOptions);
 		
 		layer.on ('click', function(e) {
-				save_event = e;
-				el.addData(e.target.feature)
+				addData(feature)
 			});
 	}
   }).addTo(map);
