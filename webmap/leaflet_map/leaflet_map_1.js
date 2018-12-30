3097e437cdb07fe70e6aefe9ec54398797825d3c
gimoya <kay.cichini@gmail.com>
2018-12-30 22:11:29 +0100

    batch updates from shell script..

--- webmap/leaflet_map/leaflet_map_1.js
+++ webmap/leaflet_map/leaflet_map_1.js
@@ -75,14 +75,14 @@ var el = L.control.elevation({
 		position: "bottomright",
 		theme: "steelblue-theme", //default: lime-theme
 		width: 600,
-		height: 150,
+		height: 200,
 		margins: {
 			top: 10,
 			right: 20,
 			bottom: 30,
 			left: 80
 		},
-		useHeightIndicator: false, //if false a marker is drawn at map position
+		useHeightIndicator: true, //if false a marker is drawn at map position
 		interpolation: "linear", //see https://github.com/mbostock/d3/wiki/SVG-Shapes#wiki-area_interpolate
 		hoverNumber: {
 			decimalsX: 3, //decimals on distance (always in km)
