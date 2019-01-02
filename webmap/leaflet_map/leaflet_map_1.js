79ef1d7d1a955c140a056f586cbcf43e174d7631
gimoya <kay.cichini@gmail.com>
2019-01-02 16:46:14 +0100

    batch updates from shell script..

--- webmap/leaflet_map/leaflet_map_1.js
+++ webmap/leaflet_map/leaflet_map_1.js
@@ -115,6 +115,7 @@ $.getJSON('Trails.json', function(json) {
 					map.removeControl(el);
 				};
 				el.addTo(map);
+				el.addData.bind(el);
 				el.addData(feature, layer);
 				L.DomEvent.stopPropagation(e);
 			});
