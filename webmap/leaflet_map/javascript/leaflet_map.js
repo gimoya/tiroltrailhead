645cc425a3bdf6eb4fe6eda36bba261ba79064c4
gimoya <kay.cichini@gmail.com>
2019-05-02 17:15:48 +0200

    batch updates from shell script..

--- webmap/leaflet_map/javascript/leaflet_map.js
+++ webmap/leaflet_map/javascript/leaflet_map.js
@@ -86,7 +86,7 @@ function select (layer) {
 	var previous = selected;
   }
 	map.fitBounds(layer.getBounds());
-	layer.setText('>                ', { repeat: true, offset: -0.1, attributes: {fill: 'white', 'font-weight': 'bold', 'font-size': '20'} });
+	layer.setText('⮞              ', { repeat: true, offset: -7, attributes: {fill: 'white', 'font-weight': 'bold', 'font-size': '15'} });
 	selected = layer;
 	if (previous) {
 	  dehighlight(previous);
