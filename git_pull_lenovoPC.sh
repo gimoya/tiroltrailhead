88a781a2477bb7f479d9bf7c5bc9b65a0c2e9517
gimoya <kay.cichini@gmail.com>
2020-12-21 18:51:39 +0100

    batch updates from shell script..

--- git_pull_lenovoPC.sh
+++ git_pull_lenovoPC.sh
@@ -4,10 +4,13 @@ cd D:/Projekte/tiroltrailhead/
 echo -e "\n---\n  Pull from Remote??\n  Hit y/n + [Enter]:"
 read yesno
 if [ "$yesno" = "y" ] 
+then
+	git status
+	sleep.exe 10
 
-git status
-sleep.exe 10
-
-git pull origin gh-pages
-
+	git pull origin gh-pages
 sleep.exe 20
+else 
+	echo -e "\n  You typed "$yesno" ..part skipped!\n---\n"
+fi
+read -p "Hit [Enter] to exit..."
