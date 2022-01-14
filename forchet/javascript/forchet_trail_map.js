3f45d14ecab20c5bb740c2e2baf422585e349460
Kay Cichini <kay.cichini@gmail.com>
2022-01-14 16:31:29 +0100

    mapbox -> mapy.cz

--- forchet/javascript/forchet_trail_map.js
+++ forchet/javascript/forchet_trail_map.js
@@ -65,9 +65,9 @@ var toggle = L.easyButton({
 
 toggle.addTo(map);
 
-var mapbox_Attr = 'Tiles &copy; <a href="https://www.mapbox.com">mapbox</a> | Design &copy; <a href="http://www.tiroltrailhead.com/guiding">Tirol Trailhead</a>';  
-var mapbox_satelliteUrl = 'https://api.mapbox.com/styles/v1/mapbox/satellite-streets-v10/tiles/256/{z}/{x}/{y}?access_token=pk.eyJ1IjoiZ2ltb3lhIiwiYSI6IkZrTld6NmcifQ.eY6Ymt2kVLvPQ6A2Dt9zAQ';
-var mapbox_outdoorUrl = 'https://api.mapbox.com/styles/v1/mapbox/outdoors-v10/tiles/256/{z}/{x}/{y}?access_token=pk.eyJ1IjoiZ2ltb3lhIiwiYSI6IkZrTld6NmcifQ.eY6Ymt2kVLvPQ6A2Dt9zAQ';
+var mapbox_Attr = 'Tiles &copy; <a href="https://www.mapy.cz">MAPY.CZ</a> | Design &copy; <a href="http://www.tiroltrailhead.com/guiding">Tirol Trailhead</a>';  
+var mapbox_satelliteUrl = 'https://mapserver.mapy.cz/ophoto-m/{z}-{x}-{y}';
+var mapbox_outdoorUrl = 'https://mapserver.mapy.cz/turist-m/{z}-{x}-{y}';
 
 var mapbox_satelliteLayer = L.tileLayer(mapbox_satelliteUrl, {
   attribution: mapbox_Attr 
