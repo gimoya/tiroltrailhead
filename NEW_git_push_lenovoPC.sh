e04418680b7951084fa60f8b4b9d447b8977354b
gimoya <kay.cichini@gmail.com>
2023-04-13 16:37:35 +0200

    batch updates from shell script..

--- NEW_git_push_lenovoPC.sh
+++ NEW_git_push_lenovoPC.sh
@@ -3,8 +3,9 @@ read yesno
 if [ "$yesno" = "y" ] 
 then
 	cd ~
-	cd d:/Projekte/tiroltrailhead/traillegacy
+	cd d:/Projekte/tiroltrailhead/tirol_legacy_trails
 	ogr2ogr my_trails_z.geojson my_trails_z_epsg3857.geojson -s_srs EPSG:900913 -t_srs EPSG:4326
+	echo -e "\n  ..reprojection to WGS84 done!\n---\n"
 else 
 	echo -e "\n  You typed "$yesno" ..part skipped!\n---\n"
 fi
