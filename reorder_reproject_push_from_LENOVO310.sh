0397e0859ecf59ebb38ace7f91dfe862b1c26593
Kay Cichini <kay.cichini@gmail.com>
2024-09-13 23:10:34 +0200

    batch updates from shell script..

--- reorder_reproject_push_from_LENOVO310.sh
+++ reorder_reproject_push_from_LENOVO310.sh
@@ -18,4 +18,16 @@ echo -e "Deleting gdalwmscache before upload!"
 echo -e "\\n---\\n" 
 rm -rf C:/GitHub/tiroltrailhead/webmap/digitizing/gdalwmscache
 
+#push to gh-pages
+
+git add -A
+git status
+git commit -m 'batch updates from shell script..'
+echo -e "\n---\n"  
+read -p "Hit [Enter] to continue..."
+git push origin gh-pages
+echo -e "\n---\n"
+
+
 read -p "Hit [Enter] to exit..."
+
