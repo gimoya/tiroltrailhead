3d98bfecbc6e49ff31cb03787de83af14cabd559
Kay Cichini <kay.cichini@gmail.com>
2024-11-07 10:21:02 +0100

    batch updates from shell script..

---
+++ git_pull_LENOVO310.sh
@@ -0,0 +1,16 @@
+cd ~
+cd C:/GitHub/tiroltrailhead
+
+echo -e "\n---\n  Pull from Remote??\n  Hit y/n + [Enter]:"
+read yesno
+if [ "$yesno" = "y" ] 
+then
+	git status
+	sleep.exe 10
+
+	git pull origin gh-pages
+sleep.exe 20
+else 
+	echo -e "\n  You typed "$yesno" ..part skipped!\n---\n"
+fi
+read -p "Hit [Enter] to exit..."
