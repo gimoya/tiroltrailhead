d66aa85a8963deafb78de5884a81cca52da2f4b6
Kay Cichini <kay.cichini@gmail.com>
2025-01-17 17:43:03 +0100

    batch updates from shell script..

--- webmap/digitizing/trails_reorder_reproject_json.bat
+++
@@ -1,6 +0,0 @@
-REM sort trails-db and save to GEOJSON
-@echo on
-cd d:/Projekte/tiroltrailhead/webmap/digitizing/
-ogr2ogr -sql "SELECT name, description, geometry FROM 'trails_utm_utf8' ORDER BY name ASC" -f GEOJSON Trails.json trails_utm_utf8.sqlite -s_srs EPSG:900913 -t_srs EPSG:4326
-
-PAUSE
