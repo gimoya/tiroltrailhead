0cd9b2f70be88d7a628aa931bdb8ab4ed1d72d61
Kay Cichini <kay.cichini@gmail.com>
2025-04-04 15:24:18 +0200

    batch updates from shell script..

--- TheHunt25/javascript/thehuntmap.js
+++ TheHunt25/javascript/thehuntmap.js
@@ -1,6 +1,6 @@
 // Add this at the start of the file, after the map initialization
 // Set release date globally
-const releaseDate = new Date('2025-04-04T14:20:00');
+const releaseDate = new Date('2025-04-04T15:30:00');
 
 // Global emoji marker
 let emojiMarker = null;
